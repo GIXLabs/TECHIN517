d37c494b70ca0d495c44b8c0e9c4d1cba4e33d09
jack <42076119+htchr@users.noreply.github.com>
2026-01-30 08:54:43 -0800

    remove submodules

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,6 +1,6 @@
 source /home/ubuntu/.bashrc
 cd /home/ubuntu/techin517/third_party/lerobot
-pip install 'lerobot[all]'
+pip install --break-system-packages 'lerobot[all]'
 cd /home/ubuntu/techin517/so101_ws/src/so101_ros2/
 bash build.sh
 source /home/ubuntu/.bashrc
